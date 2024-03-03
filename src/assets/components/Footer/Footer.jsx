3fe7f209f2725ec1ed80cc87449cfcc37d8d3c51
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-03 16:32:16 +0800

    Add footer component

---
+++ src/assets/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+  return(
+    <>
+      <footer id="footer">
+        <h1>simple web app by: bangueco</h1>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
