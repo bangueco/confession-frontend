9257885b1e864065832ed4045a7bb8367e2da887
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-03 16:31:02 +0800

    Add header component

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+  return(
+    <>
+      <header id="header">
+        <div className="logo">CONFESSION</div>
+      </header>
+    </>
+  )
+}
+
+export default Header
