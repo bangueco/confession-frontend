71c88941d2ba357db52760e81c275f09dfc415ac
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-03 16:31:34 +0800

    Add main component

---
+++ src/assets/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css'
+import Confessions from "./Confessions.jsx"
+
+const Main = () => {
+  return (
+    <>
+      <main>
+        <Confessions />
+      </main>
+    </>
+  )
+}
+
+export default Main
