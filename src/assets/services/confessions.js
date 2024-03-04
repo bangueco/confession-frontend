350a3104630d3317914a7eac5cd60cf7879667d7
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-04 21:38:55 +0800

    Update baseURL

--- src/assets/services/confessions.js
+++ src/assets/services/confessions.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001'
+const baseURL = '/api/confessions'
 
 const getAll = () => {
-  return axios.get(`${baseURL}/api/confessions`).then(response => response.data)
+  return axios.get(`${baseURL}`).then(response => response.data)
 }
 
 const sendConfession = (msg) => {
   const confessionObj = { message: msg }
-  return axios.post(`${baseURL}/api/confessions`, confessionObj).then(response => response.data)
+  return axios.post(`${baseURL}`, confessionObj).then(response => response.data)
 }
 
 export default {
